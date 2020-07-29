3f156bc1b32d2c68fdeeb7306139053d9cd14a8d
tinashekupfuwa <tinashekupfuwa@gmail.com>
2020-07-30 00:17:16 +0200

    push

--- counter-app/src/components/fig.component.jsx
+++ counter-app/src/components/fig.component.jsx
@@ -1,14 +1,1 @@
-import React from "react";
-
-class Fig extends Component {
-  constructor() {}
-  render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
-}
-
-export default Fig;
+import React, { Component } from "react";
